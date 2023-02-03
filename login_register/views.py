409c21b51b1a11938d6f24f6e48addaba1eaa8db
Shahadil Munawir <shahadilmunawir110@gmail.com>
2023-02-03 15:49:05 +0530

    add views: login, signup, reset_password, home

--- login_register/views.py
+++ login_register/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    return render(request, 'login.html', context={})
+
+def signup_view(request):
+    return render(request, 'signup.html', context={})
+
+def reset_password_view(request):
+    return render(request, 'reset_password.html', context={})
+
+def home_view(request):
+    return render(request, 'home.html', context=context)
