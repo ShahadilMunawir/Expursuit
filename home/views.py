99d55d1ae699713bf24b506de36c0f40aaf48178
Shahadil Munawir <shahadilmunawir110@gmail.com>
2023-02-05 12:07:52 +0530

    add home_view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request):
+    spending = 10000
+    budget = 20000
+    percentage = int((spending/budget)*100)
+    context = {
+        "name": "Shahadil Munawir",
+        "spending": 10000,
+        "budget": 20000,
+        "percentage": percentage
+    }
+    return render(request, 'home.html', context=context)
