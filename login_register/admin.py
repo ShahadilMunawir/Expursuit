e1f1b956cb6d303a73d5bdb1542ec0695dc50fa7
Shahadil Munawir <shahadilmunawir110@gmail.com>
2023-02-25 14:08:23 +0530

    register: User

--- login_register/admin.py
+++ login_register/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import UserSignUp
+from .models import User
 
 # Register your models here.
 
-admin.site.register(UserSignUp)
+admin.site.register(User)
