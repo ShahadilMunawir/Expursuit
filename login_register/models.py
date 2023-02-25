94fe4dc147762722a851c9130117ada0111b06f9
Shahadil Munawir <shahadilmunawir110@gmail.com>
2023-02-25 14:09:29 +0530

    create model: User

--- login_register/models.py
+++ login_register/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    fullname = models.TextField()
+    username = models.TextField()
+    password = models.TextField()
+    email = models.TextField()
+    budget = models.TextField()
