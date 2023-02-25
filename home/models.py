6f15f06fea3eabcf1e63a728a06579bf61e69f1d
Shahadil Munawir <shahadilmunawir110@gmail.com>
2023-02-25 11:45:28 +0530

    create model: Expense

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Expense(models.Model):
+    userId = models.TextField()
+    category = models.TextField()
+    amount = models.TextField()
+    paymentType = models.TextField()
+    date = models.TextField()
+    description = models.TextField()
