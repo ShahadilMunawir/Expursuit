f64aa62a24ab3c5c1704a0cf2882ed4ab7820be6
ShahadilMunawir <shahadilmunawir110@gmail.com>
2023-03-11 01:38:55 +0530

    change field name

--- login_register/models.py
+++ login_register/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    fullname = models.TextField()
-    username = models.TextField()
-    password = models.TextField()
+    fullName = models.TextField()
+    userName = models.TextField()
+    passwordHash = models.TextField()
     email = models.TextField()
     budget = models.TextField()
-    profilePicture = models.ImageField(null=True, blank=True, upload_to="images/")
+    profilePicture = models.ImageField(null=True, blank=True, upload_to="images/profile picture")
