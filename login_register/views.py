3000c61e0128249513d98c11cbf960ccc14ad851
ShahadilMunawir <shahadilmunawir110@gmail.com>
2023-03-11 01:40:25 +0530

    add: password encryption, decryption

--- login_register/views.py
+++ login_register/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import render, HttpResponse, redirect
 from django.core.handlers.wsgi import WSGIRequest
 from login_register.models import User
@@ -13,13 +14,18 @@ def login_view(request: WSGIRequest):
         username = request.POST["username"]
         password = request.POST["password"]
 
-        if User.objects.filter(username=username, password=password):
-            messages.success(request, f"{username} logged in")
-            request.session["userId"] = User.objects.filter(username=username).values()[0]["id"]
-            print(User.objects.filter(username=username).values()[0]["id"])
-            return redirect(home_view)
+        if User.objects.filter(username=username):
+            passwordHash = User.objects.filter(username=username)[0].passwordHash
+            
+            if check_password(password, passwordHash):
+                messages.success(request, f"{username} logged in")
+                request.session["userId"] = User.objects.filter(username=username).values()[0]["id"]
+                print(User.objects.filter(username=username).values()[0]["id"])
+                return redirect(home_view)
+            else:
+                messages.error(request, f"Password is wrong for {username}")
         else:
-            messages.error(request, f"{username} not found or wrong password")
+            messages.error(request, f"{username} does not exist")
     return render(request, 'login.html', context={})
     
 def signup_view(request: WSGIRequest):
@@ -31,8 +37,10 @@ def signup_view(request: WSGIRequest):
         budget = request.POST["budget"]
         profilePicture = request.FILES["profile-picture"]
         
+        passwordHash = make_password(password)
+
         if not User.objects.filter(username=username):
-            user = User(fullname=fullname, username=username, password=password, email=email, budget=budget, profilePicture=profilePicture)
+            user = User(fullname=fullname, username=username, passwordHash=passwordHash, email=email, budget=budget, profilePicture=profilePicture)
             user.save()
             messages.success(request, f"{username} has been registered succesfully")
         else:
